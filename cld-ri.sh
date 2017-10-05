1ebd2c02b3c6f1733674a2932eb192efa136b4fb
Julio Faerman <jfaerman@gmail.com>
2017-10-05 16:21:38 -0300

    Just pipe it already

--- cld-ri.sh
+++ cld-ri.sh
@@ -7,3 +7,8 @@ aws cloudformation package \
     --template-file cld-ri-src.yaml \
     --s3-bucket cld-ri-packages \
     --output-template-file cld-ri.yaml
+
+aws cloudformation deploy \
+    --template-file cld-ri.yaml \
+    --stack-name cld-ri-stack \
+    --capabilities CAPABILITY_IAM
